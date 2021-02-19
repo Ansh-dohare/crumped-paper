826b848cfcc8115ebefd43ebf43f9f9a0cf2c997
Ansh-dohare <76939009+Ansh-dohare@users.noreply.github.com>
2021-02-19 00:18:21 -0800

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,36 @@
+class Paper{   
+ constructor(x, y, radius){
+    var options = {
+        isStatic: false,
+        'restitution': 0.3,
+        'friction': 0,
+        'density': 1.2
+    }
+    this.radius = radius;
+    this.body = Bodies.circle(x, y, (this.radius-20)/2, options)
+  
+    this.image = loadImage("paper.png")
+
+    World.add(world, this.body)
+ }
+
+display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y)
+    
+    imageMode(CENTER);
+    fill("yellow");
+   // circle(this.body.position.x, this.body.position.y, this.radius)
+    image(this.image, 0, 0, this.radius, this.radius);
+    pop();
+}
+
+}
+
+
+
+
+
+
+
